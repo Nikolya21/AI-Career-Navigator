203e3348d4412e03ad0ca4866a58d3d092b89f46
Nikolya Khachatryan <164537426+Nikolya21@users.noreply.github.com>
2025-11-26 00:12:57 +0300

    correct file

--- src/main/java/com/aicareer/servlet/LoginServlet.java
+++ src/main/java/com/aicareer/servlet/LoginServlet.java
@@ -13,6 +13,11 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet{
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+    request.getRequestDispatcher("/jsp/login.jsp").forward(request, response);
+  }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) 
     throws ServletException, IOException {
